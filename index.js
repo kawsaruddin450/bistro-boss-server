dd6963a0460bb09ac7c476bedb3cfbeb9b3301a5
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-01 15:35:43 +0600

    verify jwt

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" });
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 app.get('/', (req, res) => {
     res.send(`Bistro Boss Server is running at: ${port}`)
 })
@@ -43,10 +59,10 @@ async function run() {
         });
 
         //jwt
-        app.post('/jwt', (req, res)=> {
+        app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
-            res.send({token});
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
         })
 
         //get all users
@@ -87,12 +103,18 @@ async function run() {
         });
 
         //get cart data of specific user
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             let query = {}
             if (!email) {
                 res.send([]);
             }
+            
+            const decodedEmail = req.decoded.email;
+            if(decodedEmail !== email){
+                return res.status(403).send({error: true, message: "forbidden access"});
+            }
+
             else if (email) {
                 query = { email: email };
             }
